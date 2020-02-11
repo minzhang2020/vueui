5e99c6b6ab501463d4683d1f8eb4acba953b01c3
zhangmin <zhrm1984@163.com>
2020-02-11 20:33:06 +0800

    增加样式设置

---
+++ packages/themes/gulpfile.js
@@ -0,0 +1,16 @@
+const stylus = require('gulp-stylus')
+const gulp = require('gulp')
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCss = require('gulp-clean-css')
+const rename = require('gulp-rename')
+function build() {
+  return gulp
+    .src('./src/index.styl')
+    .pipe(stylus())
+    .pipe(autoprefixer())
+    .pipe(cleanCss())
+    .pipe(rename('vu.css'))
+    .pipe(gulp.dest('../../dist/styles'))
+}
+
+gulp.task('default', build)
